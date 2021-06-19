ab7c774c525ecfd106152804a3abb4b574d1d185
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-06-18 20:24:58 -0700

    Working on delete route.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,13 +4,36 @@ const notes = require('../db/db.json');
 // ROUTING
 module.exports = (app) => {
   app.get('/api/notes', (req, res) => res.json(notes));
+
   app.post('/api/notes', (req, res) => {
     const {title, text, id} = req.body;
-    notes.push({title, text, id}); res.json(req.body);
+    notes.push(req.body); 
+    res.json(notes);
   })
+
   app.delete('/api/notes/:id', (req, res) => {
-    let id = req.params.id.toString();
-    notes.filter((note) => note.id !== id)
-    .then((filteredNotes) => JSON.stringify(filteredNotes)); res.json(filteredNotes);
+    .removeNote(req.params.id);
+    .then() => res.json(notes);
+    .catch (err)
   })
 };
+
+// router.delete('/:id', async (req, res) => {
+//   try {
+//     const projectData = await Project.destroy({
+//       where: {
+//         id: req.params.id,
+//         user_id: req.session.user_id,
+//       },
+//     });
+
+//     if (!projectData) {
+//       res.status(404).json({ message: 'No project found with this id!' });
+//       return;
+//     }
+
+//     res.status(200).json(projectData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
